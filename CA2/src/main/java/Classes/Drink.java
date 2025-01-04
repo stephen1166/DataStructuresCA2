9e307dea68ab91f6640d1281001574423980000e
enderlord116 <20102455@mail.wit.ie>
2025-01-04 21:37:45 +0000

    Update Drink.java

--- CA2/src/main/java/Classes/Drink.java
+++ CA2/src/main/java/Classes/Drink.java
@@ -1,4 +1,48 @@
 package Classes;
 
+import java.net.URL;
+
 public class Drink {
+
+    private String name,origin,texture;
+    private URL image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getTexture() {
+        return texture;
+    }
+
+    public void setTexture(String texture) {
+        this.texture = texture;
+    }
+
+    public URL getImage() {
+        return image;
+    }
+
+    public void setImage(URL image) {
+        this.image = image;
+    }
+
+    public Drink(String n,String o,String t,URL i){
+        name=n;
+        origin=o;
+        texture=t;
+        image=i;
+    }
 }
