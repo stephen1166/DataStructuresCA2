aed6573c03019a580d670188902cf2e442f17f35
James <20102342@mail.wit.ie>
2025-01-05 23:25:43 +0000

    Drink.java, drinks-view.fxml, DrinksView.java initial commit

--- CA2/src/main/java/Classes/Drink.java
+++ CA2/src/main/java/Classes/Drink.java
@@ -4,45 +4,73 @@ import java.net.URL;
 
 public class Drink {
 
-    private String name,origin,texture;
+    private String name;
+    private String type;
+    private String origin;
+    private String description;
     private URL image;
 
+    //Constructor
+    public Drink(String name, String type, String origin, String description, URL image) {
+        this.name = name;
+        this.type = type;
+        this.origin = origin;
+        this.description = description;
+        this.image = image;
+    }
+
+    //Getters
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getType() {
+        return type;
     }
 
     public String getOrigin() {
         return origin;
     }
 
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public String getTexture() {
-        return texture;
-    }
-
-    public void setTexture(String texture) {
-        this.texture = texture;
+    public String getDescription() {
+        return description;
     }
 
     public URL getImage() {
         return image;
     }
 
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public void setImage(URL image) {
         this.image = image;
     }
 
-    public Drink(String n,String o,String t,URL i){
-        name=n;
-        origin=o;
-        texture=t;
-        image=i;
+    //toString
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Drink{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", origin='" + origin + '\'' +
+                ", description='" + description + '\'' +
+                ", image=" + image +
+                '}';
     }
 }
