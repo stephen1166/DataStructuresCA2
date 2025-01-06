f6d449721ca1c4dbf190342ba92aaf933cc53c4a
James <20102342@mail.wit.ie>
2025-01-06 13:46:47 +0000

    Comments on Hashmap.java

--- CA2/src/main/java/Util/Hashmap.java
+++ CA2/src/main/java/Util/Hashmap.java
@@ -11,22 +11,25 @@ public class Hashmap<E> {
     public int hash(E item) {
         int total = 0;
         for (int i = 0; i < item.toString().length(); i++) {
-            total += item.toString().charAt(i);
+            total += item.toString().charAt(i); //Adds the value of the ASCII character to the total
         }
+
+        //Makes sure that the indexes are within the bounds of the hash
         return total % hashTable.length;
     }
 
     public void insert(E value) {
         int index = hash(value);
         for (int i = 0; i <= hashTable.length; i++) {
+            //If the selected index reaches then end of the table go back to the start and try again
             if (hashTable[index] != null) {
                 if (index >= hashTable.length - 1) {
                     index = 0;
                 } else {
-                    index++;
+                    index++; //if the selected index is full go to the next one
                 }
             } else {
-                hashTable[index] = value;
+                hashTable[index] = value; //Insert value into the index
                 return;
             }
         }
